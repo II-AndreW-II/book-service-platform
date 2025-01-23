1eb3ed8ed5f3f60271e9111d30c2ee4d94323d3c
AndreW <andrey.melkunas@gmail.com>
2025-01-23 07:44:40 +0300

    Edited findMembers function

--- src/main/java/com/ifortex/bookservice/service/impl/MemberServiceImpl.java
+++ src/main/java/com/ifortex/bookservice/service/impl/MemberServiceImpl.java
@@ -7,13 +7,16 @@ import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
     private static final String ROMANCE_GENRE = "Romance";
-    private static final Integer YEAR_OF_REGISTRATION = 2023;
+    LocalDateTime START_DATE_OF_REGISTRATION = LocalDate.of(2023, 1, 1).atStartOfDay();
+    LocalDateTime END_DATE_OF_REGISTRATION = LocalDate.of(2023, 12, 31).atTime(23, 59, 59);
 
     @PersistenceContext
     private final EntityManager em;
@@ -39,11 +42,13 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public List<Member> findMembers() {
         return em.createQuery("""
-                        SELECT Member FROM Member
-                        LEFT JOIN FETCH Member.borrowedBooks
-                        WHERE Member.borrowedBooks IS NULL AND YEAR(Member.membershipDate) = ?1
-                        """, Member.class)
-                .setParameter(1, YEAR_OF_REGISTRATION)
+                    SELECT m FROM Member m
+                    LEFT JOIN FETCH m.borrowedBooks
+                    WHERE SIZE(m.borrowedBooks) = 0 
+                      AND m.membershipDate BETWEEN :startDate AND :endDate
+                    """, Member.class)
+                .setParameter("startDate", START_DATE_OF_REGISTRATION)
+                .setParameter("endDate", END_DATE_OF_REGISTRATION)
                 .getResultList();
     }
 }
